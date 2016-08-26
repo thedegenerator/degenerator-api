86c399712605dfc7fb73ed290414eaf155206532
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2016-08-26 16:58:21 -0500

    images post to the main page and are degenerating

--- app/Model/Upload.js
+++ app/Model/Upload.js
@@ -1,10 +1,13 @@
-'use strict'
+'use strict';
 
-const Lucid = use('Lucid')
+const Lucid = use('Lucid');
 
 class Upload extends Lucid {
 
+  user() {
+    return this.belongsTo('App/Model/User');
+  }
 
 }
 
-module.exports = Upload
+module.exports = Upload;
